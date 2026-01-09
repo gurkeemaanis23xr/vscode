389bdca3c14f33064925ea7420de382c3f3bec53
Zhichao Li <zhichli@microsoft.com>
2026-01-09 11:47:01 -0800

    adding branch name and removing workspace file count

--- src/vs/workbench/contrib/chat/common/model/chatModel.ts
+++ src/vs/workbench/contrib/chat/common/model/chatModel.ts
@@ -1295,8 +1295,8 @@ export interface ISerializableMarkdownInfo {
 export interface IExportableRepoData {
 	remoteUrl: string | undefined;
 	repoType: 'github' | 'ado' | 'other';
+	branchName: string | undefined;
 	headCommitHash: string | undefined;
-	workspaceFileCount: number;
 	changedFileCount: number;
 	diffs: IExportableRepoDiff[] | undefined;
 }
